436354aefac803d7f8e2f2fbe699ddf50e5acf11
Alisson Youssf <youssfbr@gmail.com>
2021-05-06 16:23:18 -0300

    Fix seed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -9,10 +9,10 @@ INSERT INTO tb_specialty (name, description, active) VALUES ('Pediatria', 'Crian
 INSERT INTO tb_specialty (name, description, active) VALUES ('Nefrologia', 'Nervos',1);
 INSERT INTO tb_specialty (name, description, active) VALUES ('Neurologia', 'Cérebro',1);
 
-INSERT INTO tb_doctor (name, active) VALUES ('João Felipe', 1);
-INSERT INTO tb_doctor (name, active) VALUES ('Janaina Souza', 1);
-INSERT INTO tb_doctor (name, active) VALUES ('Raimundo Girão', 1);
-INSERT INTO tb_doctor (name, active) VALUES ('Lisontro Souza', 1);
+INSERT INTO tb_doctor (name, birth_date, active) VALUES ('João Felipe', '1980-10-10', 1);
+INSERT INTO tb_doctor (name, birth_date, active) VALUES ('Janaina Souza', '1978-05-22', 1);
+INSERT INTO tb_doctor (name, birth_date, active) VALUES ('Raimundo Girão', '1990-03-02', 1);
+INSERT INTO tb_doctor (name, birth_date, active) VALUES ('Lisontro Souza', '1962-07-18', 1);
 
 INSERT INTO tb_doctor_specialty (doctor_id, specialty_id) VALUES (1, 1);
 INSERT INTO tb_doctor_specialty (doctor_id, specialty_id) VALUES (1, 6);
