234efcee52fbfb0193324a9a787ab1dcdd3eab00
Alisson Youssf <youssfbr@gmail.com>
2021-05-06 13:30:27 -0300

    Project created

---
+++ backend/src/main/java/com/github/youssfbr/medcad/MedcadApplication.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.medcad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedcadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedcadApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/youssfbr/medcad/MedcadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.medcad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedcadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
